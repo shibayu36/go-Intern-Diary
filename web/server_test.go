3e9a8185d9a02688f95c3e81b92d6e382aa9f91e
shibayu36 <shibayu36@gmail.com>
2019-06-03 09:56:12 +0900

    indexの簡易テストを追加

--- web/server_test.go
+++ web/server_test.go
@@ -2,6 +2,13 @@ package web
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hatena/go-Intern-Diary/config"
+	"github.com/hatena/go-Intern-Diary/repository"
+	"github.com/hatena/go-Intern-Diary/service"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -11,4 +18,28 @@ func init() {
 	csrfToken = func(r *http.Request) string {
 		return ""
 	}
+}
+
+func newAppServer() (service.DiaryApp, *httptest.Server) {
+	conf, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	repo, err := repository.New(conf.DbDsn)
+	if err != nil {
+		panic(err)
+	}
+	app := service.NewApp(repo)
+	handler := NewServer(app).Handler()
+	return app, httptest.NewServer(handler)
+}
+
+func TestServer_Index(t *testing.T) {
+	_, ts := newAppServer()
+	defer ts.Close()
+
+	resp, respBody := client.Get(ts.URL + "/").Do()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Contains(t, respBody, `<h1>ダイアリー</h1>`)
 }
