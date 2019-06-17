7db26c598ec87fdf192ab7eac93780fbe1c36a7a
shibayu36 <shibayu36@gmail.com>
2019-06-17 09:26:27 +0900

    diaryとarticleテーブル

--- db/schema.sql
+++ db/schema.sql
@@ -25,3 +25,34 @@ CREATE TABLE user_session (
 
     PRIMARY KEY (token)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE diary (
+    `id` BIGINT UNSIGNED NOT NULL,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+
+    `name` VARBINARY(32) NOT NULL,
+
+    `created_at` DATETIME(6) NOT NULL,
+    `updated_at` DATETIME(6) NOT NULL,
+
+    PRIMARY KEY (id),
+    KEY (user_id),
+    KEY (created_at),
+    KEY (updated_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE article (
+    `id` BIGINT UNSIGNED NOT NULL,
+    `diary_id` BIGINT UNSIGNED NOT NULL,
+
+    `title` VARBINARY(100) NOT NULL,
+    `body` MEDIUMBLOB NOT NULL,
+
+    `created_at` DATETIME(6) NOT NULL,
+    `updated_at` DATETIME(6) NOT NULL,
+
+    PRIMARY KEY (id),
+    KEY (diary_id),
+    KEY (created_at),
+    KEY (updated_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
