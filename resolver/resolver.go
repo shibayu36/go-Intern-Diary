9436376d3f31dc9f7f07f05a211020b49231e00c
shibayu36 <shibayu36@gmail.com>
2019-10-11 21:30:49 +0900

    Visitorはログインしてなければnullなはず

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -2,6 +2,7 @@ package resolver
 
 import (
 	"context"
+	"errors"
 
 	"github.com/hatena/go-Intern-Diary/model"
 	"github.com/hatena/go-Intern-Diary/service"
@@ -20,10 +21,15 @@ type resolver struct {
 	app service.DiaryApp
 }
 
-func currentUser(ctx context.Context) *model.User {
-	return ctx.Value("user").(*model.User)
+func currentUser(ctx context.Context) (*model.User, error) {
+	user := ctx.Value("user").(*model.User)
+	if user == nil {
+		return nil, errors.New("visitor is not found")
+	}
+	return user, nil
 }
 
 func (r *resolver) Visitor(ctx context.Context) (*userResolver, error) {
-	return &userResolver{currentUser(ctx), r.app}, nil
+	user, err := currentUser(ctx)
+	return &userResolver{user, r.app}, err
 }
