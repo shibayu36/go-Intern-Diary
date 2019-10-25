162802e10dd2d338b11e4a0799e8713b0efc23c3
shibayu36 <shibayu36@gmail.com>
2019-10-25 10:35:00 +0900

    global headerにvisitorを出してみる

--- ui/src/components/global_header.tsx
+++ ui/src/components/global_header.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import gql from "graphql-tag";
+import { useQuery } from '@apollo/react-hooks'
 
-export const GlobalHeader = () => (
-  <header className="GlobalHeader">
+const visitorQuery = gql`
+  query GetVisitor {
+    visitor {
+      id
+      name
+    }
+  }
+`;
+
+export const GlobalHeader = () => {
+  const { loading, error, data } = useQuery(visitorQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  console.log(error)
+
+  return <header className="GlobalHeader">
+    ようこそ、{data.visitor.name}さん
     <nav>
       <ul>
         <li><NavLink to="/">トップ</NavLink></li>
       </ul>
     </nav>
   </header>
-);
+};
