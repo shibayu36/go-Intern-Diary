d7652def6e6c9e1d8d88d71eb9736ab5e519ce6f
shibayu36 <shibayu36@gmail.com>
2019-10-25 23:29:28 +0900

    gqlに型付ける

---
+++ ui/src/components/__generated__/GetVisitor.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetVisitor
+// ====================================================
+
+export interface GetVisitor_visitor {
+  readonly __typename: "User";
+  readonly id: string;
+  readonly name: string;
+}
+
+export interface GetVisitor {
+  readonly visitor: GetVisitor_visitor;
+}
--- ui/src/components/diaries.tsx
+++ ui/src/components/diaries.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
+import { GetMyDiaries } from "./__generated__/GetMyDiaries"
 
 const MyDiariesQuery = gql`
   query GetMyDiaries {
@@ -16,15 +17,15 @@ const MyDiariesQuery = gql`
 `
 
 export const Diaries: React.StatelessComponent = () => {
-  const { loading, error, data } = useQuery(MyDiariesQuery);
+  const { loading, error, data } = useQuery<GetMyDiaries>(MyDiariesQuery);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
-  const diaries = data.visitor.diaries;
+  const diaries = data!.visitor.diaries;
 
   return <div className="Diaries">
-    <h1>{data.visitor.name}のダイアリー一覧</h1>
+    <h1>{data!.visitor.name}のダイアリー一覧</h1>
     {diaries.map((diary: any) => (
       <div><p>{diary.name}</p></div>
     ))}
