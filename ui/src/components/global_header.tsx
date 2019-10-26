778c74aeb87f55856f18c8b65343758a3fff2582
shibayu36 <shibayu36@gmail.com>
2019-10-26 11:31:41 +0900

    getVisitorにも型付ける

--- ui/src/components/global_header.tsx
+++ ui/src/components/global_header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import gql from "graphql-tag";
 import { useQuery } from '@apollo/react-hooks'
+import { GetVisitor } from "./__generated__/GetVisitor";
 
 const getVisitorQuery = gql`
   query GetVisitor {
@@ -13,13 +14,13 @@ const getVisitorQuery = gql`
 `;
 
 export const GlobalHeader = () => {
-  const { loading, error, data } = useQuery(getVisitorQuery);
+  const { loading, error, data } = useQuery<GetVisitor>(getVisitorQuery);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
   return <header className="GlobalHeader">
-    ようこそ、{data.visitor.name}さん
+    ようこそ、{data!.visitor.name}さん
     <nav>
       <ul>
         <li><NavLink to="/">トップ</NavLink></li>
